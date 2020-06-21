a2ab991839687c78dab345c0097d315e80e7449c
Jameel Hassan <jhassan@Jameels-MacBook-Pro.local>
2020-06-21 22:48:06 +0530

    Ackermann full code with plots

--- Ackermann_steer.m
+++ Ackermann_steer.m
@@ -17,7 +17,7 @@ velnois =2;          %Velocity input noise
 u = 10;     %Velocity of 10 cm/s
 
 %System model
-x = [0;0;0.2];    %Initial condition
+x = [0;0;0];    %Initial condition
 xhat = [0;0;0]; %Initial estimatd
 A = eye(3);     %System matrix
 B = [dt*cos(x(3)); dt*sin(x(3)); tan(phi)*dt/L];    %Input matrix
@@ -75,7 +75,48 @@ for t = 1:dt:dur
 end
 
 t = 1:dt:dur;
-plot(poshat(:,1),poshat(:,2),'g-', 'Linewidth', 1.5)
+
+%Plots
+figure();
+plot(pos(:,1),pos(:,2),'color',[0, 0, 1],'Linewidth', 1.5)
 hold on
-plot(pos(:,1),pos(:,2),'color',[0.8500, .250, 0],'Linewidth', 1.5)
-scatter(posmeas(:,1), posmeas(:,2),10,'m','filled')
+plot(poshat(:,1),poshat(:,2),'g-', 'Linewidth', 1.5)
+scatter(posmeas(:,1), posmeas(:,2),10,'r','filled')
+legend('Actual position', 'Estimated position','Measured position');
+xlabel('x axis position/ cm');
+ylabel('y axis position/ cm');
+title('Position of Ackerman steering robot')
+grid on;
+
+figure()
+plot(t, pos(:,3), 'b-', t, poshat(:,3), 'g-', t, posmeas(:,3),'ro', 'MarkerSize',2);
+legend('Actual angle', 'Estimated angle','Measured angle');
+xlabel('Time/ secs');
+ylabel('Angle \theta/ rad');
+title('Variation of angle \theta')
+grid on;
+
+figure();
+subplot(3,1,1)
+plot(t, pos(:,1)-posmeas(:,1),'r-', t, pos(:,1)-poshat(:,1), 'g-');
+legend('Error in measurement', 'Error in estimation');
+grid on;
+xlabel('Time/ sec');
+ylabel('Error/ cm');
+title('Error in x position measurement');
+
+subplot(3,1,2)
+plot(t, pos(:,2)-posmeas(:,2),'r-', t, pos(:,2)-poshat(:,2), 'g-');
+legend('Error in measurement', 'Error in estimation');
+grid on;
+xlabel('Time/ sec');
+ylabel('Error/ cm');
+title('Error in y position measurement');
+
+subplot(3,1,3)
+plot(t, pos(:,3)-posmeas(:,3),'r-', t, pos(:,3)-poshat(:,3), 'g-');
+legend('Error in measurement', 'Error in estimation');
+grid on;
+xlabel('Time/ sec');
+ylabel('Error/ rad');
+title('Error in angle \theta measurement');
